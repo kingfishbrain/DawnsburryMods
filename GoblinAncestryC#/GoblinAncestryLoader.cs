604d492136d3598bb80b35e6add31c9e0407d715
King <thomasmaennlein1@gmail.com>
2024-03-19 13:36:30 +0100

    change falling damage line

--- GoblinAncestryC#/GoblinAncestryLoader.cs
+++ GoblinAncestryC#/GoblinAncestryLoader.cs
@@ -214,7 +214,7 @@ public static class GoblinAncestryLoader
 
         yield return new HeritageSelectionFeat(FeatName.CustomFeat,
                "You're able to bounce back from injuries easily due to an exceptionally thick skull, cartilaginous bones, or some other mixed blessing. ",
-               " You gain 10 Hit Points from your ancestry instead of 6. If there was falling damage in this game, you would reduce the falling damage you take as though you had fallen half the distance.")
+               " You gain 10 Hit Points from your ancestry instead of 6. If the third dimension existed in this game, you would reduce the falling damage you take as though you had fallen half the distance.")
            .WithCustomName("Unbreakable Goblin")
            .WithOnCreature(creature => creature.MaxHP += 4);
     }
