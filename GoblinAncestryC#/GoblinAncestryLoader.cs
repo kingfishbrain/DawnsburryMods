54aaf5b4cd8e3d9418a90a754d9b55349a2ac84b
King <thomasmaennlein1@gmail.com>
2024-03-23 00:18:10 +0100

    fix Burn It! parenthesis

--- GoblinAncestryC#/GoblinAncestryLoader.cs
+++ GoblinAncestryC#/GoblinAncestryLoader.cs
@@ -122,7 +122,7 @@ public static class GoblinAncestryLoader
                "Fire fascinates you.",
                "Your spells and alchemical items that deal fire damage gain a status bonus to damage equal to half the spell's level" +
                "or one-quarter the item's level (minimum 1). " +
-               "You also gain a +1 status bonus to any persistent fire damage you deal.
+               "You also gain a +1 status bonus to any persistent fire damage you deal.")
            .WithOnCreature(goblin =>
            {
                var burnIt = new QEffect("Burn It!", "Adds damage to fire spells")
