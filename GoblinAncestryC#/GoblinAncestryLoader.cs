7bb2bcab5763346948447809768c1a463ab39a36
King <thomasmaennlein1@gmail.com>
2024-03-22 20:00:56 +0100

    fix missing parenthesis

--- GoblinAncestryC#/GoblinAncestryLoader.cs
+++ GoblinAncestryC#/GoblinAncestryLoader.cs
@@ -228,7 +228,7 @@ public static class GoblinAncestryLoader
         yield return new HeritageSelectionFeat(FeatName.CustomFeat,
                "Your ancestors have always had a connection to fire and a thicker skin, which allows you to resist burning.",
                "You gain fire resistance equal to half your level (minimum 1). You can also recover from being on fire more easily. " +
-               "Your flat check to remove persistent fire damage is DC 10 instead of DC 15."
+               "Your flat check to remove persistent fire damage is DC 10 instead of DC 15.")
            .WithCustomName("Charhide Goblin")
            .WithOnCreature((sheet, creature) =>
            {
