f68f14c3539210bb56c9dfa16b7e8dd19b63e05b
King <thomasmaennlein1@gmail.com>
2024-10-24 15:11:13 +0200

    Adjust Boarslicer and add Horsechopper

--- DawnsburryMods/GoblinAncestry/Weapons.cs
+++ DawnsburryMods/GoblinAncestry/Weapons.cs
@@ -12,8 +12,11 @@ public static class Weapons
     public static void RegisterWeapons()
     {
         ModManager.RegisterNewItemIntoTheShop("Boarslicer", itemName =>
-        new Item(itemName, IllustrationName.Dogslicer, "Boarslicer", 0, 3, Trait.TwoHanded, Trait.Backstabber, Trait.Agile, GoblinAncestryLoader.GoblinTrait, Trait.Finesse, Trait.Weapon, Trait.Homebrew, Trait.Martial, Trait.Sword)
+        new Item(itemName, IllustrationName.Dogslicer, "Boarslicer", 0, 3, Trait.TwoHanded, Trait.Backstabber, Trait.Goblin, Trait.Finesse, Trait.Weapon, Trait.Homebrew, Trait.Martial, Trait.Sword)
             .WithWeaponProperties(new WeaponProperties("1d8", DamageKind.Slashing))
-            .WithDescription("Sometimes you need to slice something bigger than a dog."));
+            .WithDescription("Sometimes you need to slice something bigger than a dog."));        ModManager.RegisterNewItemIntoTheShop("Boarslicer", itemName =>
+        new Item(itemName, IllustrationName.Halberd, "Horsechopper", 0, 9, Trait.TwoHanded, Trait.Trip, Trait.Goblin, Trait.Reach, Trait.Weapon, Trait.Martial, Trait.Polearm, Trait.VersatileP)
+            .WithWeaponProperties(new WeaponProperties("1d8", DamageKind.Slashing))
+            .WithDescription("Created by goblins to battle horses, this weapon is essentially a long shaft ending in a blade with a large hook."));
     }
 }
