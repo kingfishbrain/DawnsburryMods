8a7fce3b4bade2d743a2aabb3eaa38ba88bb9e89
King <thomasmaennlein1@gmail.com>
2025-08-16 23:02:34 +0200

    remove weapons code because someone on the workshop finally made a mod that adds a ton of missing weapons

--- DawnsburryMods/DistractingFeint.cs
+++ DawnsburryMods/DistractingFeint.cs
@@ -30,25 +30,8 @@ namespace DawnsburryMods
             giveFeintStep();
             addRogueMartialProficiency();
             addRuffianMartialSneakAttack();
-        // addWeapons();
-
         }
 
-        private static void addWeapons()
-        {
-            ModManager.RegisterNewItemIntoTheShop("Dancer's Spear", itemName =>
-            new Item(itemName, IllustrationName.Glaive, "Dancer's Spear", 0, 3, Trait.TwoHanded, Trait.Backswing, Trait.Finesse, Trait.Reach, Trait.Weapon, Trait.Martial, Trait.Spear, Trait.VersatileB, Trait.Sweep)
-                .WithWeaponProperties(new WeaponProperties("1d6", DamageKind.Piercing))
-                .WithDescription("Originally developed for an elaborate dancing performance, it was discovered it made for an effective weapon after an unfortunate accident."));
-            ModManager.RegisterNewItemIntoTheShop("Elven Branched Spear", itemName =>
-            new Item(itemName, IllustrationName.Glaive, "Elven Branched Spear", 0, 3, Trait.TwoHanded, Trait.DeadlyD8, Trait.Finesse, Trait.Reach, Trait.Weapon, Trait.Martial, Trait.Spear)
-                .WithWeaponProperties(new WeaponProperties("1d6", DamageKind.Piercing))
-                .WithDescription("Several short branches project from this delicate spear's shaft, each angled forward and tipped with a leaflike blade."));
-            ModManager.RegisterNewItemIntoTheShop("Boarding Pike", itemName =>
-                 new Item(itemName, IllustrationName.Longspear, "Boarding Pike", 0, 1, Trait.TwoHanded, Trait.Shove, Trait.Reach, Trait.Weapon, Trait.Martial, Trait.Polearm)
-                .WithWeaponProperties(new WeaponProperties("1d10", DamageKind.Piercing))
-                .WithDescription("Taking the form of a longspear fitted with crossbars or hooks, a boarding pike provides its wielder a sharp implement that's as adept at shoving enemies off a ship's railings as facilitating the boarding of other vessels. "));
-        }
 
         private static void addRuffianMartialSneakAttack()
         {
